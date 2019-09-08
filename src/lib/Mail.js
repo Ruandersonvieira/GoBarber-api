de7884cb14df5109cd168bddaa360d046ff3578f
Ruanderson Vieira <ruanderson.vieira@gmail.com>
2019-09-08 06:15:12 -0300

    :sparkles: personalize html email

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,4 +1,7 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import expressHandlebars from 'express-handlebars';
+import nodemailerHandlebars from 'nodemailer-express-handlebars';
 import mailconfig from '../config/mail';
 
 class Mail {
@@ -10,6 +13,26 @@ class Mail {
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerHandlebars({
+        viewEngine: expressHandlebars.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs', // default: .handlebars
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendMail(message) {
